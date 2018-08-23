be0673d6bd247dcda736bc4bf1dc09dd34c344fd
nirmala <nirmala@surf.net.np>
2018-08-23 17:31:38 +0200

    add js, css files to Cell lesson

--- app/javascript/scimo/index.js
+++ app/javascript/scimo/index.js
@@ -2,5 +2,7 @@ import "./styles/application.css";
 import "./styles/style.css";
 import "./styles/Indicators.css";
 import "./styles/DNA.css";
+import "./styles/Cells.css";
 
 import "./scripts/DNA.js";
+import "./scripts/Cells.js";
