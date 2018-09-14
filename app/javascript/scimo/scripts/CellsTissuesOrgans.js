1fc01250808ccf911a9f7b8d0a999960482c1cf1
nirmala <nirmala@surf.net.np>
2018-09-14 19:25:21 +0200

    file chages

---
+++ app/javascript/scimo/scripts/CellsTissuesOrgans.js
@@ -0,0 +1,35 @@
+function makeCell() {
+    var posx = (Math.random() * ($(".tissue-container").width())).toFixed();
+    var posy = (Math.random() * ($(".tissue-container").height())).toFixed();
+    $newCell = $("<div class='cell-clone'></div>").clone().css({
+        'left': posx + 'px',
+        'top': posy + 'px',
+        
+    });
+
+    $newCell.appendTo('.tissue-container');
+}
+
+
+function multiplyCell(){
+    for(var i = 0; i < 120; i++){
+        setTimeout(function timer(){
+            makeCell();
+        }, i * 65); 
+    }           
+}
+
+
+setTimeout( multiplyCell, 11000 );
+
+$(function(){
+    $('.organ').hide().delay(22000).queue(function(n) {
+        $(this).show(); n();
+    })
+});
+
+$(function(){
+$('.organ-system').hide().delay(26000).queue(function(n) {
+    $(this).show(); n();
+})
+});
